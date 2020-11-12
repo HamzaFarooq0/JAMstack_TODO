58400dc23496524c46f17f623c7317baa4a1c109
Hamza Farooq <hamza3366123@gmail.com>
2020-11-12 08:12:52 +0500

    updated theme-ui && netlifyIdentity

--- gatsby_todo/src/pages/index.js
+++ gatsby_todo/src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React from 'react';
+
+//Theme-UI
+import { Container, Heading, Button, Flex } from 'theme-ui';
+
+//importing netlifyIdentity
+import netlifyIdentity from 'netlify-identity-widget'
+
+//Initializing netlifyidentity
+netlifyIdentity.init({});
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<Container>
+			<Flex sx={{ flexDirection: 'column', padding: 3 }}>
+				<Heading as="h1">Theme-ui Container and theme checking !!</Heading>
+				<Button sx={{ margin: 2 }} onClick={() => netlifyIdentity.open()}>
+					Add Task
+				</Button>
+			</Flex>
+		</Container>
+	);
 }
